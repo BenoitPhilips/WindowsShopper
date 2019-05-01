422d6100239e62b89ab04d508234b6f2149dfd28
BenoitPhilips <Benoit.Philips@GMail.com>
2019-05-01 15:09:07 +0700

    added 2 textfields code driven

--- Windows Shopper/Controllers/MainVC.swift
+++ Windows Shopper/Controllers/MainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC.swift
 //  Windows Shopper
 //
 //  Created by Benoit Philips on 30/04/2019.
@@ -8,13 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC : UIViewController {
 
+    
+    @IBOutlet weak var wageTxt: CurrencyTextField!
+    @IBOutlet weak var priceTxt: CurrencyTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        calcBtn.backgroundColor = #colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1)
+        calcBtn.setTitle("Calculate", for: .normal)
+        calcBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calcBtn.addTarget(self, action: #selector(MainVC.doCalculate), for: .touchUpInside)
+
+       wageTxt.inputAccessoryView = calcBtn
+        priceTxt.inputAccessoryView = calcBtn
+        
     }
 
-
+    @objc func doCalculate() {
+        print("BPH was heer")
+    }
+    
+ 
 }
 
+fileprivate extension Selector {
+    static let calculate = #selector(MainVC.doCalculate)
+}
--- Windows Shopper/Views/CurrencyTextField.swift
+++ Windows Shopper/Views/CurrencyTextField.swift
@@ -8,14 +8,29 @@
 
 import UIKit
 
+@IBDesignable
+
 class CurrencyTextField: UITextField {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override func prepareForInterfaceBuilder() {
+        customizeView()
     }
-    */
-
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+       customizeView()
+      }
+    
+    func customizeView () {
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2497592038)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        
+//        if let p = placeholder {
+//            let place = NSAttributedString(string: p, attributes: [.foregroundColor :  #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)])
+//            attributedPlaceholder = place
+//            textColor =  #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+//        }
+    }
 }
