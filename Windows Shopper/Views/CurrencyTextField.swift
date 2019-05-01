3f191903b1d8545d9f79eac2d21cf4edf07f8e8e
BenoitPhilips <Benoit.Philips@GMail.com>
2019-05-01 15:07:13 +0700

    Added two code driven text fields

---
+++ Windows Shopper/Views/CurrencyTextField.swift
@@ -0,0 +1,21 @@
+//
+//  CurrencyTextField.swift
+//  Windows Shopper
+//
+//  Created by Benoit Philips on 30/04/2019.
+//  Copyright © 2019 HumbeekWave. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
